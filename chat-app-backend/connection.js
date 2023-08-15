bc8f20b9fbd982806d6304aa5b627109df6b6d46
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-08-15 06:20:27 +0600

    fixed-database-connection

--- chat-app-backend/connection.js
+++ chat-app-backend/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
  const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://dmabuhasib:dmabuhasib@cluster0.wwa72fu.mongodb.net/ChatAppData')
+        await mongoose.connect('mongodb+srv://dmabuhasib:dmabuhasib@cluster0.wwa72fu.mongodb.net/ChattAppData')
         console.log("db is connected")
     } catch (error) {
         console.log("db is not connected")
